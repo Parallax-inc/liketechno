e19c0002b93071574374580137bc57a2f80be534
roman120487 <roman120487@ukr.net>
2020-02-28 00:22:42 +0200

    Revert "Revert "Merge branch 'Roman' into vika""
    This reverts commit a741c02dd4dd3ff486fb6bc97e4f4f06200e5eb0.

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  brends = [
+    {img: "assets/img/brends/msi.png"},
+    {img: "assets/img/brends/dell.png"},
+    {img: "assets/img/brends/fj.gif"},
+    {img: "assets/img/brends/hp.png"},
+    {img: "assets/img/brends/lenovo.jpg"},
+    {img: "assets/img/brends/medion.png"},
+    {img: "assets/img/brends/pb.png"}
+  ];
+  slides = [
+    {img: "assets/img/sliderMain/1.png"},
+    {img: "assets/img/sliderMain/2.png"},
+    {img: "assets/img/sliderMain/3.png"},
+  ];
 
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 1000,
+    infinite: true,
+    lazyLoad: 'ondemand',
+    dots: true,
+    fade: true,
+    pauseOnHover: false,
+    arrows: false
+  };
+  slideConfigBrends = {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 1000,
+    infinite: true,
+    lazyLoad: 'ondemand',
+    dots: false,
+    fade: false,
+    pauseOnHover: false,
+    arrows: false,
+    // variableWidth: true
+  };
   constructor() { }
 
   ngOnInit() {
+    console.log(this.slides);
+    
   }
 
 }
