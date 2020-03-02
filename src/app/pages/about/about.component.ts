a8dd070563e1ce0d83c8a5aa7666908f39c463b8
Viktoriia Dakh <dakhviktoriia@users.noreply.github.com>
2020-03-02 16:38:38 +0200

    commit  abt

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  showMore: boolean = true;
+
   constructor() { }
 
+
   ngOnInit() {
+  }
+
+  public clickMore(): void {
+    console.log('dfghnm,');
   }
 
 }
